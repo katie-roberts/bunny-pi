7270e28fed841c78fc22c72caf9834ff319630e7
Katie Stockton Roberts <katie.roberts@bbc.co.uk>
2017-12-03 21:44:53 +0000

    lighting test - this works ok.  Motor test is currently untested but the pin names are based on what i have seen at https://github.com/adafruit/Adafruit-Motor-HAT-Python-Library/blob/master/Adafruit_MotorHAT/Adafruit_MotorHAT_Motors.py#L174

---
+++ motor-test.js
@@ -0,0 +1,93 @@
+var five = require("johnny-five"),
+  board = new five.Board();
+
+board.on("ready", function() {
+  var leftEar;
+  var rightEar;
+
+
+  leftEar = new five.Motor({
+    pins: {
+      pwm: 8,
+      dir: 9,
+      enable: 10
+    },
+    invertPWM: true
+  });
+
+  rightEar = new five.Motor({
+    pins: {
+      pwm: 2,
+      dir: 3,
+      enable: 4
+    },
+    invertPWM: true
+  });
+
+  board.repl.inject({
+    leftEar: leftEar,
+    rightEar: rightEar
+  });
+
+  leftEar.on("stop", function() {
+    console.log("automated stop on timer", Date.now());
+  });
+
+  leftEar.on("forward", function() {
+    console.log("forward", Date.now());
+
+    // enable the motor after 2 seconds
+    board.wait(2000, function() {
+      leftEar.enable();
+    });
+  });
+
+  leftEar.on("enable", function() {
+    console.log("motor enabled", Date.now());
+
+    // enable the motor after 2 seconds
+    board.wait(2000, function() {
+      leftEar.stop();
+    });
+  });
+
+  leftEar.on("disable", function() {
+    console.log("motor disabled", Date.now());
+  });
+
+
+  rightEar.on("stop", function() {
+    console.log("automated stop on timer", Date.now());
+  });
+
+  rightEar.on("forward", function() {
+    console.log("forward", Date.now());
+
+    // enable the motor after 2 seconds
+    board.wait(2000, function() {
+      rightEar.enable();
+    });
+  });
+
+  rightEar.on("enable", function() {
+    console.log("motor enabled", Date.now());
+
+    // enable the motor after 2 seconds
+    board.wait(2000, function() {
+      rightEar.stop();
+    });
+  });
+
+  rightEar.on("disable", function() {
+    console.log("motor disabled", Date.now());
+  });
+
+
+  // disable the motor
+  leftEar.disable();
+  rightEar.disable();
+
+  // set the motor going forward full speed (nothing happen)
+  leftEar.forward(255);
+  rightEar.forward(255);
+});
