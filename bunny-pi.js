00e272cb4a18e4228d464589cedbad975175cb55
katie-roberts <katie.roberts@bbc.co.uk>
2018-08-16 13:55:07 +0100

    adding in express basic

--- bunny-pi.js
+++ bunny-pi.js
@@ -9,7 +9,7 @@ const board = new five.Board({
 
 const app = express ();
 
-app.post('/bunny-commands', function request, response {
+app.post('/bunny-commands', function (request, response) {
   console.log("howdy")
 })
 
